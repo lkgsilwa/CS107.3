2c7bde910e7558e2ca315a43bbb8bbee20356aef
kulan geeneth <147399756+lkgsilwa@users.noreply.github.com>
2024-01-31 15:10:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labsheet1.q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of terms (N): ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("First {0} terms of Fibonacci series: ", n);
+            PrintFibonacci(n);
+            Console.WriteLine();
+        }
+        static void PrintFibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                Console.Write("{0}", n);
+            }
+            else
+            {
+                PrintFibonacci(n - 1);
+                Console.Write("{0}", n );
+            }
+        }
+    }
+}
